2a7338224338f629b497d4fdb19f4e2f55367bce
Kaviya <732125104051.25.cse@nandhatech.org>
2026-07-07 06:16:33 +0530

    Create Main19.java

---
+++ Main19.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        int price=2000;
+        int discountpercentage=10;
+        int discount=(price*discountpercentage)/100;
+        System.out.println("Discount: "+discount);
+    }
+}
